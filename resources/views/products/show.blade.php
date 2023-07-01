84c1dde7dbc5d2708a7693799d614ae3a9fd8e65
Muhammad Mashuri <uraey.1289@gmail.com>
2023-07-01 12:52:39 +0700

    First commit

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,41 @@
+@extends('products.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NIK:</strong>
+                {{ $product->NIK }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                {{ $product->Nama }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                {{ $product->Alamat }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Details:</strong>
+                {{ $product->detail }}
+            </div>
+        </div>
+    </div>
+@endsection
